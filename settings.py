90dca97651b5b583542131e9e2176b9867def0e2
Andrew Barr <wabarr@gmail.com>
2022-03-26 20:18:33 +0000

    update secret key

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!-6%$mm&wxxegjoxxvnl4bfesf31_!k#@nad3_rq-q(zkl)cs'
+SECRET_KEY = supersecrets.SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
